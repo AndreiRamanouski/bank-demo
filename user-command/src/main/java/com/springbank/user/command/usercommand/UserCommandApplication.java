b9a9abb102053817aa58883e065289d1e0329d07
Andrei Romanouski <@aromanouski1>
2023-06-25 11:58:37 +0300

    [Bank demo] - setup axon

--- user-command/src/main/java/com/springbank/user/command/usercommand/UserCommandApplication.java
+++ user-command/src/main/java/com/springbank/user/command/usercommand/UserCommandApplication.java
@@ -1,8 +1,11 @@
 package com.springbank.user.command.usercommand;
 
+import com.springbank.user.core.usercore.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import({AxonConfig.class})
 @SpringBootApplication
 public class UserCommandApplication {
 
--- user-query/src/main/java/com/springbanking/user/query/userquery/UserQueryApplication.java
+++ user-query/src/main/java/com/springbanking/user/query/userquery/UserQueryApplication.java
@@ -1,8 +1,12 @@
 package com.springbanking.user.query.userquery;
 
+import com.springbank.user.core.usercore.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+
+@Import({AxonConfig.class})
 @SpringBootApplication
 public class UserQueryApplication {
 
