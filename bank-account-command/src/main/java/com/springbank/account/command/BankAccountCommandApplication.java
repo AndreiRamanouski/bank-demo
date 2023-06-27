2e6f8ae071b4b54c42528fe7a9091b898225dc2b
Andrei Romanouski <@aromanouski1>
2023-06-27 09:04:34 +0300

    [BANK-ACCOUNT] - initial commit for 3 modules

---
+++ bank-account-command/src/main/java/com/springbank/account/command/BankAccountCommandApplication.java
@@ -0,0 +1,13 @@
+package com.springbank.account.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountCommandApplication.class, args);
+    }
+
+}
