aa8e94c6e446c41158dc9eda3f8de4a21c7ab222
Andrei Ramanouski <68558820+AndreiRamanouski@users.noreply.github.com>
2023-06-27 09:14:58 +0400

    Mocked security (#4)
    Co-authored-by: Andrei Romanouski <@aromanouski1>

--- user-command/src/main/java/com/springbank/user/command/usercommand/UserCommandApplication.java
+++ user-command/src/main/java/com/springbank/user/command/usercommand/UserCommandApplication.java
@@ -1,11 +1,12 @@
 package com.springbank.user.command.usercommand;
 
 import com.springbank.user.core.usercore.configuration.AxonConfig;
+import com.springbank.user.core.usercore.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import({AxonConfig.class})
+@Import({AxonConfig.class, SecurityConfig.class})
 @SpringBootApplication
 public class UserCommandApplication {
 
--- user-query/src/main/java/com/springbanking/user/query/userquery/UserQueryApplication.java
+++ user-query/src/main/java/com/springbanking/user/query/userquery/UserQueryApplication.java
@@ -1,12 +1,13 @@
 package com.springbanking.user.query.userquery;
 
 import com.springbank.user.core.usercore.configuration.AxonConfig;
+import com.springbank.user.core.usercore.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 
-@Import({AxonConfig.class})
+@Import({AxonConfig.class, SecurityConfig.class})
 @SpringBootApplication
 public class UserQueryApplication {
 
